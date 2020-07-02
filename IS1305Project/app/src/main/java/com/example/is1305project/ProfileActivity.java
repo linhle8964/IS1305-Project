6dcd28bead2d35296a830d46562c8a5cb7ae25c0
taducbinh123 <52749957+taducbinh123@users.noreply.github.com>
2020-07-02 13:51:57 +0700

    get user profile(non-edit)

--- IS1305Project/app/src/main/java/com/example/is1305project/ProfileActivity.java
+++ IS1305Project/app/src/main/java/com/example/is1305project/ProfileActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -16,9 +18,25 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
     GoogleSignInClient mGoogleSignInClient;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private ImageView avatar;
+    private TextView nameTV, EmailTV;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +46,35 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Profile");
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("Users");
+        avatar = findViewById(R.id.avatar);
+        nameTV = findViewById(R.id.NameTV);
+        EmailTV = findViewById(R.id.EmailTV);
+        Query query = reference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    String name = "" + ds.child("username").getValue();
+                    String email = "" + ds.child("email").getValue();
+                    String image = "" + ds.child("imageURL").getValue();
+                    nameTV.setText(name);
+                    EmailTV.setText(email);
+                    try {
+                        Picasso.get().load(image).into(avatar);
+                    } catch (Exception e) {
+                        Picasso.get().load(R.drawable.ic_add_image).into(avatar);
+                    }
+                }
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
